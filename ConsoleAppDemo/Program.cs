f404ccd19b1b2d30a7725c7b6c8cc216ed02eec1
MinHuiTian@outlook.com <MinHuiTian@outlook.com>
2019-06-18 18:57:42 +0800

    webAPI 基础

---
+++ ConsoleAppDemo/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestNewGreetingAdd();
+
+            Test();
+        }
+
+        public void HostTset()
+        {
+            //System.Web.Http.SelfHost.dll
+
+            //var config = new HttpSelfHostConfiguration(new Uri("http://localhost:10930"));
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate:"api/{controller}/{id}",
+            //    defaults :new { id = RouteParameter.Optional});
+
+            //var host = new HttpSelfHostServer(config);
+            //host.OpenAsync().Wait();
+
+            //Console.WriteLine("Press any key to exit");
+            //Console.ReadKey();
+
+            //host.CloseAsync().Wait();
+        }
+
+        /// <summary>
+        /// 模拟客户端调用
+        /// </summary>
+        public static void Test()
+        {
+            var greetingServiceAddress = new Uri("http://localhost:10930/api/greeting");
+            var client = new HttpClient();
+            var result = client.GetAsync(greetingServiceAddress).Result;
+            var greeting = result.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(greeting);
+        }
+
+        //[Fact]
+        //public void TestNewGreetingAdd()
+        //{
+        //    //准备
+        //    var greetingName = "newgreeting";
+        //    var greetingMessage = "Hello Test";
+        //    var fakeRequest = new HttpRequestMessage(HttpMethod.Post, "http://localhost:10930/api/greeting");
+        //    var greeting = new Greeting
+        //    {
+        //        Name = greetingName,
+        //        Message=greetingMessage
+        //    };
+
+        //    var service = new GreetingController();
+        //    service.Request = fakeRequest;
+
+        //    //操作
+        //    var response = service.PostGreeting(greeting);
+
+        //    //断言
+        //    Assert.NotNull(response);
+        //    Assert.Equal(HttpStatusCode.Created,response.StatusCode);
+        //    Assert.Equal(new Uri("http://localhost:10930/api/greeting/newgreeting"), response.Headers.Location);
+        //}
+
+        //模拟客户端调用
+    }
+}
--- WebApi/Controllers/GreetingController.cs
+++ WebApi/Controllers/GreetingController.cs
@@ -3,17 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using WebApi.Models;
 
 namespace WebApi.Controllers
 {
     public class GreetingController : ApiController
     {
+        public static List<Greeting> _greetings = new List<Greeting>();
+
         public string GetGreeting()
         {
-            return "Hello World!";
+            return "Hello World!"; 
         }
 
+        public string GetGreeting(string id)
+        {
+            var greeting = _greetings.FirstOrDefault(g => g.Name == id);
+            if (greeting == null)
+            {
+                //throw new HttpResponseException(HttpStatusCode.NotFound);
+                throw new HttpResponseException(this.Request.CreateErrorResponse(HttpStatusCode.NotFound,"没有对应的数据！"));
+            }
+
+            return greeting.Message;
+        }
+
+        public HttpResponseMessage PostGreeting(Greeting greeting)
+        {
+            _greetings.Add(greeting);
+
+            Uri greetingLocation = new Uri(this.Request.RequestUri, "greeting/" + greeting.Name);
+            HttpResponseMessage response = this.Request.CreateResponse(HttpStatusCode.Created);
+            response.Headers.Location = greetingLocation;
+            
+            return response;
+        }
     }
-    
 }
